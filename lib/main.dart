b848fc382267d75bec704f1d47a7f1f831c8442d
fawaz-alesayi <fawaztsa@gmail.com>
2022-02-19 12:14:39 +0300

    feature: base layout for app  1️⃣

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              fontFamily: 'Oregano', color: Colors.white, fontSize: 48),
+          headline2:
+              TextStyle(fontFamily: 'Kanit', color: Colors.white, fontSize: 28),
+        ),
+        primarySwatch: const MaterialColor(0xFF390099, {
+          50: Color(0xFFE7E0F3),
+          100: Color(0xFFC4B3E0),
+          200: Color(0xFF9C80CC),
+          300: Color(0xFF744DB8),
+          400: Color(0xFF5726A8),
+          500: Color(0xFF390099),
+          600: Color(0xFF330091),
+          700: Color(0xFF2C0086),
+          800: Color(0xFF24007C),
+          900: Color(0xFF17006B),
+        }),
       ),
       home: const HomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
@@ -23,7 +40,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   final String title;
 
-
   const HomePage({
     Key? key,
     required this.title,
@@ -31,18 +47,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Render a text in the middle of the screen
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-          style: Theme.of(context).textTheme.headline4,
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Container(
+        padding: const EdgeInsets.all(32),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Center(
+                child: Text(
+                  'Special Invitations!',
+                  style: Theme.of(context).textTheme.headline1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Center(
+                child: Text(
+                  'Choose your template',
+                  style: Theme.of(context).textTheme.headline2,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            Placeholder(fallbackHeight: 200, color: Colors.white),
+            const SizedBox(height: 32),
+            Placeholder(fallbackHeight: 200, color: Colors.white),
+            const SizedBox(height: 32),
+            Placeholder(fallbackHeight: 200, color: Colors.white)
+          ],
         ),
       ),
     );
   }
-  
 }
